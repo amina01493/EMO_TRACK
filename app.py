112617ff84338eb63c50e223ee136f92e6115b0e
simplehima <ebrahimwaleedazab@gmail.com>
2026-05-15 19:04:40 +0300

    build: increment version to 1.0.5+1005 in pubspec.yaml

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import secrets
 import random
 import string
 # Version Control
-APP_VERSION = "1.0.6"
+APP_VERSION = "1.0.5"
 MIN_COMPATIBLE_VERSION = "1.0.0"
 
 def generate_unique_code(length=6):
@@ -855,7 +855,7 @@ def api_app_version():
         'version': cached_github_version,
         'local_version': APP_VERSION,
         'min_compatible': MIN_COMPATIBLE_VERSION,
-        'build_number': 5,
+        'build_number': 1005,
         'release_date': datetime.now().strftime('%Y-%m-%d'),
         'github_repo': GITHUB_REPO
     })
